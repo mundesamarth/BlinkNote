5c12b77a295ab47dffe6a3da854386032143c10d
Sam <gentalmen28@gmail.com>
2025-05-29 17:55:19 +0100

    Header setup is done

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,35 +1,35 @@
-import Link from "next/link";
 import { ScrollText } from "lucide-react";
 import { Button } from "../ui/button";
+import NavLink from "./nav-link";
 
 export default function Header() {
     const isLoggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
-        <Link href="/" className="flex items-center  gap-1 lg:gap-2 shrink-0 ">
+        <NavLink href="/" className="flex items-center  gap-1 lg:gap-2 shrink-0 ">
           <ScrollText className="w-5 h-5 lg:w-8 h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
-          <span className="font-extrabold lg:text-xl text-gray-900">
+          <span className=" font-extrabold lg:text-xl text-gray-900">
             BlinkNote
           </span>
-        </Link>
+        </NavLink>
       </div>
 
-      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
-        <Link href="/#pricing">Pricing</Link>
-        {isLoggedIn && <Link href="/dashboard">Your Summaries</Link>}
+      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center ">
+        <NavLink href="/#pricing">Pricing</NavLink>
+        {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
       </div>
 
       <div className="flex lg:justify-end lg:flex-1">
         {isLoggedIn ? (
             <div className="flex lg:justify-end lg:flex-1">
-                <Link href="/upload">Upload a PDF</Link>
+                <NavLink href="/upload">Upload a PDF</NavLink>
                 <div>PRO</div>
                 <Button>Sign Out</Button>
             </div>
         ) : (
             <div>
-                <Link href="/sign-in">Sign In</Link>
+                <NavLink href="/sign-in">Sign In</NavLink>
             </div>
         )}
       </div>
--- components/common/nav-link.tsx
+++ components/common/nav-link.tsx
@@ -1,9 +1,32 @@
-import Link from "next/link";
+'use client'
 
-export default function NavLink({ children, href }: { children: React.ReactNode; href: string }) {
-    return (
-        <Link href={href} className="hover:underline">
-            {children}
-        </Link>
-    );
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export default function NavLink({
+  children,
+  href,
+  className,
+}: {
+  children: React.ReactNode;
+  href: string;
+  className?: string;
+}) {
+    const pathname = usePathname();
+    const isActive = pathname === href || (
+        href !== '/' && pathname.startsWith(href)
+    )
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "transition-colors text-sm duration-200  text-gray-600 hover:text-purple-500",
+        className,
+        isActive && "text-purple-500"
+      )}
+    >
+      {children}
+    </Link>
+  );
 }
