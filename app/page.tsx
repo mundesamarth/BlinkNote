853cae696d113443bfa27feb62448ec465dcf9f8
Sam <gentalmen28@gmail.com>
2025-05-29 16:27:43 +0100

    Inital final setup and all the dependencies have been installed

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16  sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    HOME
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16  sm:p-20 font-(family-name:--font-geist-sans)">
+    <h1>BlinkNote</h1>
     <Button variant={"outline"} size={"lg"}>Button</Button>
     </div>
   );
