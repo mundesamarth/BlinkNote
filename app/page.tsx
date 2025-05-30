c6f9c07a9c29e2c96752efeb7883a0cf590e5d00
Sam <gentalmen28@gmail.com>
2025-05-30 16:29:24 +0100

    Demo-section almost done, now how it works section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import HeroSection from "@/components/home/hero-section";
 import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/home/demo-section";
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <BgGradient>
+      <BgGradient />
+      <div className="flex flex-col">
         <HeroSection />
-      </BgGradient>
-      {/* <DemoSection /> */}
+        <DemoSection />
+      </div>
+
       {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
