8f9904880b7fe61d5166667205489e375bdf56ed
Sam <gentalmen28@gmail.com>
2025-05-30 18:44:25 +0100

    how-it-work section done, now pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/home/hero-section";
 import BgGradient from "@/components/common/bg-gradient";
 import DemoSection from "@/components/home/demo-section";
+import HowItWorksSection from "@/components/common/how-it-works";
 
 export default function Home() {
   return (
@@ -9,11 +10,10 @@ export default function Home() {
       <div className="flex flex-col">
         <HeroSection />
         <DemoSection />
+        <HowItWorksSection />
       </div>
-
-      {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
-  );
+  ); 
 }
