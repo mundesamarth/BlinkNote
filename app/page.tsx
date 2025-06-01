9963c5a0ddbf39711deea1ef720a7a1b7f8d31f4
Sam <gentalmen28@gmail.com>
2025-06-01 15:32:56 +0100

    Pricing section design done, now working on CTA section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/home/hero-section";
 import BgGradient from "@/components/common/bg-gradient";
 import DemoSection from "@/components/home/demo-section";
 import HowItWorksSection from "@/components/common/how-it-works";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
@@ -11,8 +12,8 @@ export default function Home() {
         <HeroSection />
         <DemoSection />
         <HowItWorksSection />
+      <PricingSection />
       </div>
-      {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
   ); 
