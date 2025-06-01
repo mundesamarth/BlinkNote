c1c8da04a803a7f4cc033e47e08b0507fdae9f9c
Sam <gentalmen28@gmail.com>
2025-06-01 21:32:24 +0100

    Authentication done, now working on upload the pdf section

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,13 +1,16 @@
 import { ScrollText } from "lucide-react";
 import { Button } from "../ui/button";
 import NavLink from "./nav-link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
-    const isLoggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
-        <NavLink href="/" className="flex items-center  gap-1 lg:gap-2 shrink-0 ">
+        <NavLink
+          href="/"
+          className="flex items-center  gap-1 lg:gap-2 shrink-0 "
+        >
           <ScrollText className="w-5 h-5 lg:w-8 h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
           <span className=" font-extrabold lg:text-xl text-gray-900">
             BlinkNote
@@ -17,21 +20,25 @@ export default function Header() {
 
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center ">
         <NavLink href="/#pricing">Pricing</NavLink>
-        {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
+        <SignedIn>
+          <NavLink href="/dashboard">Your Summaries</NavLink>
+        </SignedIn>
       </div>
 
       <div className="flex lg:justify-end lg:flex-1">
-        {isLoggedIn ? (
-            <div className="flex lg:justify-end lg:flex-1">
-                <NavLink href="/upload">Upload a PDF</NavLink>
-                <div>PRO</div>
-                <Button>Sign Out</Button>
-            </div>
-        ) : (
-            <div>
-                <NavLink href="/sign-in">Sign In</NavLink>
-            </div>
-        )}
+        <SignedIn>
+          <div className="flex lg:justify-end lg:flex-1">
+            <NavLink href="/upload">Upload a PDF</NavLink>
+            <div>PRO</div>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </SignedIn>
+
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+        </SignedOut>
       </div>
     </nav>
   );
