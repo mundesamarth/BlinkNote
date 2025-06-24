0a3aba726a3b27a5555b0b4a0f9cb255268aff30
Sam <gentalmen28@gmail.com>
2025-06-24 12:58:39 +0100

    changes to eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -6,11 +5,14 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-// ESLint (flat-config) expects an array here
-export default [
+const eslintConfig = {
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ["next/core-web-vitals", "next/typescript"]
   }),
-];
+}
+
+export default eslintConfig;
