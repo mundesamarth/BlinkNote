f4dd08b26ab66b90fa9d01859a06be7a9acc2860
Sam <gentalmen28@gmail.com>
2025-06-28 01:23:44 +0100

    Changes to the sign up page

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <section className="flex justify-center items-center lg:min-h-[40vh]">
       <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
         <BgGradient className="from-teal-400 via-teal-300 to-blue-200" />
-        <SignUp />
+        <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
       </div>
     </section>
   );
